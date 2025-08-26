624cc20e7a73bf8ac2fee8f092ba5f99cb7a1029
Tom Howes <thomas.howes01@gmail.com>
2025-08-26 13:10:51 -0400

    added city markers based on longitude, latitude and population filter

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,43 +1,48 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap, GeoJSON } from 'react-leaflet';
-import L from 'leaflet';
-import marker from '../assets/boston.svg';
-import mapData from '../assets/countries.json'
 import 'leaflet/dist/leaflet.css';
-import '../components/map.css'
-type json = typeof import('../assets/countries.json')
-const startPos: [number, number] = [39.833, -98.583];
-var CityIcon = new L.Icon({
-    iconUrl: marker,
-    iconRetinaUrl: marker,
-    iconAnchor: [16, 22],
-    iconSize: [32, 45]
-}
-);
+import cities from '../assets/cities.json';
 
-let countryStyle = {
-    fillColor: "blue",
-    fillOpacity: 0.1
+const startPos: [number, number] = [39.833, -98.583];
+
+const bigCities = cities.filter(city => Number(city['population']) > 500000);
+
+class City {
+    name: string;
+    coordinates: [number, number];
+
+    constructor(name: string, lat: number, long: number) {
+        this.name = name;
+        this.coordinates = [lat, long];
+    }
 }
+
+
+
+let finalCities: City[];
+
+finalCities = bigCities.map(x => new City(x['city'], Number(x['latitude']), Number(x['longitude'])))
 
 function Map() {
     return (
-        <MapContainer style={{ height: "90vh" }} zoom={4} center={startPos}>
-            <GeoJSON style={countryStyle} data={(mapData as json).features} />
-        </MapContainer>
+        <>
+            < MapContainer center={startPos} zoom={4} scrollWheelZoom={false} >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    finalCities.map((city) => (
+                        <Marker position={city.coordinates} key={city.name}>
+                            <Popup>
+                                {city.name}
+                            </Popup>
+                        </Marker>
+                    )
+                    )
+                }
+            </MapContainer >
+        </>
     )
-
 }
 
 export default Map;
-// Light no writing map
-//     return (<MapContainer center={position} zoom={4} scrollWheelZoom={false}>
-//     <TileLayer
-//         attribution='&copy; <a href="https://carto.com/">Carto</a>'
-//         url="http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
-//     />
-//     <Marker position={position} icon={CityIcon}>
-//         <Popup>
-//             This is a popup that will say the city name
-//         </Popup>
-//     </Marker>
-// </MapContainer>
