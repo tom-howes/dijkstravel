8fd65f941f55c210663b099fce058fea73034fd9
Tom Howes <thomas.howes01@gmail.com>
2025-09-05 19:35:25 -0400

    restored changes

--- src/components/cityIcon.tsx
+++ src/components/cityIcon.tsx
@@ -3,11 +3,23 @@ import { Marker } from 'react-leaflet';
 import './map'
 import L from 'leaflet';
 import City from './city';
-import chicago from '../assets/images/chicago.png';
+import chicagoImg from '../assets/images/Chicago.png';
+import nyImg from '../assets/images/new york.png';
+import laImg from '../assets/images/los angeles.png';
+import phillyImg from '../assets/images/philadelphia.png';
+import sanAntonioImg from '../assets/images/san antonio.png';
+
+const cityImages: { [key: string]: string } = {
+    'Chicago': chicagoImg,
+    'New York': nyImg,
+    'Los Angeles': laImg,
+    'Philadelphia': phillyImg,
+    'San Antonio': sanAntonioImg
+}
 
 const cityIcon = (city: City) => {
     return L.icon({
-        iconUrl: city.image,
+        iconUrl: cityImages[city.name] || city.image,
         iconSize: [30, 30],
         iconAnchor: [30 / 2, 30 / 2]
     })
